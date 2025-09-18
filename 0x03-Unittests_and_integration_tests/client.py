ef4cdb6421f0d2e2e5977ca51bc783bb94b0b278
jtaren <arenjatau@gmail.com>
2025-09-18 11:46:02 +0100

    client

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""A github org client"""
+"""A github org client
+"""
 from typing import (
-        List,
-        Dict,
-        )
+    List,
+    Dict,
+)
 
 from utils import (
-        get_json,
-        access_nested_map,
-        memoize,
-        )
+    get_json,
+    access_nested_map,
+    memoize,
+)
+
 
 class GithubOrgClient:
-    """A Github org client"""
+    """A Githib org client
+    """
     ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
@@ -22,11 +25,11 @@ class GithubOrgClient:
     @memoize
     def org(self) -> Dict:
         """Memoize org"""
-        return get_json(self.ORG_URL.format(org=self.org_name))
+        return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
     def _public_repos_url(self) -> str:
-        """Public repo URL"""
+        """Public repos URL"""
         return self.org["repos_url"]
 
     @memoize
@@ -38,19 +41,18 @@ class GithubOrgClient:
         """Public repos"""
         json_payload = self.repos_payload
         public_repos = [
-                repo["name"] for repo in json_payload
-                if license is None or self.haslicence(repo, license)
-                ]
+            repo["name"] for repo in json_payload
+            if license is None or self.has_license(repo, license)
+        ]
+
         return public_repos
 
     @staticmethod
     def has_license(repo: Dict[str, Dict], license_key: str) -> bool:
         """Static: has_license"""
-        assert license_key is not None, "license key cannot be None"
+        assert license_key is not None, "license_key cannot be None"
         try:
             has_license = access_nested_map(repo, ("license", "key")) == license_key
         except KeyError:
             return False
         return has_license
-
-
