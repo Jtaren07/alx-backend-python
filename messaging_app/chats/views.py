69c9bbf91726e9c95229547002bab0af978f442f
jtaren <arenjatau@gmail.com>
2025-09-20 12:12:04 +0100

    views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from .models import *
+from .setializers import MessageSerializer, ConversationSerializer
+from .permissions import import IsParticipantOfConversation
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response iport Response
+from .pagination import StandardResultsSetPagination
+from .filters import MessageFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+class ConversationViewSet(viewssets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+
+    def get_queryset(self):
+        # Show only conversations the user participates in
+        return Conversation.objects.filter(participants=self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MessageFilter
+    pagination_class = StandardResultsSetPagination
+
+    def gey_queryset(self):
+        conversation_id = self.kwargs.get('conversation_id')
+        return Message.objects.filter(
+                conversation__id = conversation_id,
+                conversation__participants = self.request.user
+            )
+
+    def perform_create(self, serializer):
+        conversation_id = self.kwargs.get('conversation_id')
+        conversation = get_object_or_404(Conversation, id=conversation_id)
+        serializer.save(sender=self.request.user, conversation=conversation)
