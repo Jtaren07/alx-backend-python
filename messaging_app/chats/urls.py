a0ef3c9f5ef5141115612515c1b1e37796798501
jtaren <arenjatau@gmail.com>
2025-09-21 21:37:04 +0100

    urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -8,8 +8,8 @@ router = DefaultRouter()
 # router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-        path('', include(router.urls))
-        ]
+    path('', include(router.urls)),
+]
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -17,16 +17,20 @@ from rest_framework.reverse import reverse
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    """API root endpoint that lists all available endpoints."""
+    """
+    API root endpoint that lists all available endpoints.
+    """
     return Response({
-        'conversations': reverse('conversation-list', request=requesr, format=format),
-        'message': reverse('message-list', request=request, format=format),
-        'conversation_detail': reverse('conversation-detail',
-                                       kwargs={'pk': 'CONVERSATION_ID'},
-                                       request=request, format=format)
-        'message_detail': reverse('message-detail', kwargs={'pk': 'MESSAGE_ID'},
-                                  request=request, format=format),
-        'search_users': reverse('conversation-search-users', request=request, format=format),
-        'recent_messages': reverse('message-recent', request=request, format=format),
-        })
-
+        'conversations': reverse('conversation-list', request=request, format=format),
+        'messages': reverse('message-list', request=request, format=format),
+        'conversation_detail': reverse('conversation-detail', 
+                                     kwargs={'pk': 'CONVERSATION_ID'}, 
+                                     request=request, format=format),
+        'message_detail': reverse('message-detail', 
+                                kwargs={'pk': 'MESSAGE_ID'}, 
+                                request=request, format=format),
+        'search_users': reverse('conversation-search-users', 
+                              request=request, format=format),
+        'recent_messages': reverse('message-recent', 
+                                 request=request, format=format),
+    })
