ef02696265451e6902ce15ac1c072a309a13f3ec
jtaren <arenjatau@gmail.com>
2025-09-22 20:44:34 +0100

    filters

---
+++ messaging_app/chats/filters.py
@@ -0,0 +1,10 @@
+import django_filters
+from .models import Message
+
+class MessageFilter(django_filters.FilterSet):
+    created_at__gte = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='gte')
+    created_at__lte = django__filters.DateTimeFilter(field_name='created_at', lookup_expr='lte')
+    sender = django_filters.CharFilter(field_name='sender_username', lookup_expr='icontains')
+    class Meta:
+        model = Message
+        fields = ['sender', 'created_at_gte', 'created_at__lte']
